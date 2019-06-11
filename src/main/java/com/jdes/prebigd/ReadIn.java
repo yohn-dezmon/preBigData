0acba3bb0924179b0859e6bc0de37f0ab84dc2c6
Johnboi <johndesmond631@gmail.com>
2019-06-11 12:12:41 -0500

    updated with advice/solution from Jesse

--- src/main/java/com/jdes/prebigd/ReadIn.java
+++ src/main/java/com/jdes/prebigd/ReadIn.java
@@ -16,7 +16,10 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 
@@ -31,24 +34,39 @@ import java.util.Collections;
 public class ReadIn {
 	private static volatile BlockingQueue<String> queue = new ArrayBlockingQueue<String>(1000);
 	// the word is the key (string), and the value is the number of times that word has appeared! 
-	static volatile HashMap<String, Integer> wordToCounts = new HashMap<>();
+	static ConcurrentHashMap<String, AtomicInteger> wordToCounts = new ConcurrentHashMap<>();
+	
+	public static final int READER_THREADS = 5;
+	
+	
+	// pool of threads for reading files 
+	ExecutorService fileReadersPool = Executors.newFixedThreadPool(READER_THREADS);
+	
+	
+	public static final int COUNTER_THREADS = 5;
+	
+	ExecutorService countersPool = Executors.newFixedThreadPool(COUNTER_THREADS);
+	
+	// queue for adding the words to be counted...
+	LinkedBlockingQueue<String[]> wordsQueue = new LinkedBlockingQueue<String[]>();
+	
+	
+	// this keeps track of when the reading is done! :D 
+	AtomicBoolean readingFinished = new AtomicBoolean(false);
+	
 	private static String line;
 	private static String path = "/Users/HomeFolder/Desktop/DevPortfolio/UltimateGuideDataEng/ch6.txt";
 	
 //	static HashMap<Integer, String> mappedLines = new HashMap<>();
 	
-	public static synchronized void mapMethod(String line) {
+	public static synchronized String[] mapMethod(String line) {
 		// map splits each line into its words... 
 		// the number of the line is stored in the while loop...
 		line = line.toLowerCase();
 		
 		String [] words = line.split("\\W");
-		for (String word: words) {
-			try { queue.offer(word);
-		} catch (ClassCastException e) {
-			System.out.println("Error putting words into queue: "+e);
-		}
-			}
+		
+		return words;
 	}
 	
 	
@@ -71,106 +89,155 @@ public class ReadIn {
 	}
 	
 //	public static void reduce(String[] words) {
-	public static void reduce(List<String> wordsFromQueue) {
+	private void reduce(String[] words) {
 		
 		
 		// reduce does the COUNTING
 		
-		for(String word : wordsFromQueue) {
+		for(String word : words) {
 			// a get on something that didn't exist in the hashmap previously
 			// returns a value of null! (that's why we need the if statements below)
-			Integer count = wordToCounts.get(word);
+			AtomicInteger count = wordToCounts.putIfAbsent(word, new AtomicInteger(0));
 			
 			if (count == null) {
 				// no previous count 
-				wordToCounts.put(word, 1);
-			} else {
-				// increment previous count 
-				wordToCounts.put(word, count + 1);
-			}
+				count = wordToCounts.get(word);
+			} 
+			
+			count.incrementAndGet();
 		}
-		wordsFromQueue.clear();
+//		wordsFromQueue.clear();
 	}
+	
+	private void createCounterThreads(ReadIn uhhh) {
+		
+		Runnable counterRunnable = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while(readingFinished.get() == false || wordsQueue.isEmpty() == false) {
+						
+						String[] words = wordsQueue.poll(1, TimeUnit.SECONDS);
+						
+						if (words != null) {
+							reduce(words);
+						} // if 
+					} // while 
+						 
+					
+				} // try
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			} // run
+		};// Runnable
+		
+		for (int i = 0; i < COUNTER_THREADS; i++) {
+			uhhh.countersPool.submit(counterRunnable);
+			
+		} // for 
+		
+	}// createCounterThreads
+	
+	
+	public void mapFile(final BufferedReader reader, ReadIn uhhh) {
+		Runnable mapTask = () -> {
+				String line = null;
+				try {
+				while ((line = reader.readLine()) != null) {
+					String[] words = mapMethod(line);
+					
+					wordsQueue.add(words);
+				} // while
+				reader.close(); 
+				} // try
+				catch (IOException e) {
+					e.printStackTrace();
+				} // catch
+				
+			
+		}; // Runnable
+		uhhh.fileReadersPool.submit(mapTask);
+		
+	} // mapFile 
+	
+//		fileReadersPool.submit(new Runnable() {
+//			@Override
+//			public void run() {
+//			String line = null;	
+//			
+////			System.out.println("mapTask inside: " + Thread.currentThread().getName());
+//			try {
+//				while ((line = reader.readLine()) != null) {
+//				    
+//				
+//				mapMethod(line); }
+////				placeInQueue(wordsInLine);
+//			
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			}
+//			
+//		};) 
+//	}
 	
 	
 	public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException {
+		
+		
 		System.out.println("Inside : " + Thread.currentThread().getName());
 		
-		// I chose two b/c this allows time for the HashMap to be populated
-		
-		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		ReadIn uhhh = new ReadIn();
 		
 		
-		
-		List<String> wordsList = new ArrayList<String>();
-		List<String> wordsFromQueue = Collections.synchronizedList(wordsList);
+//		List<String> wordsList = new ArrayList<String>();
+//		List<String> wordsFromQueue = Collections.synchronizedList(wordsList);
 		
 		BufferedReader reader = new BufferedReader(new FileReader(path));
 		
-		String firstline = reader.readLine();
-		System.out.println(firstline);
+		
+		
+		// this is where the multithreading shall begin! 
+		uhhh.mapFile(reader, uhhh);
+		
+		uhhh.createCounterThreads(uhhh);
+		
+		uhhh.fileReadersPool.shutdown();
+		uhhh.fileReadersPool.awaitTermination(60, TimeUnit.SECONDS);
+		
+		System.out.println("Reader threads shutdown.");
+		
+		uhhh.readingFinished.set(true);
+		
+		uhhh.countersPool.shutdown();
+		uhhh.countersPool.awaitTermination(60, TimeUnit.SECONDS);
 //		int lineNum;
 //			
 //			lineNum = 0;
-			
-			Runnable mapTask = () -> {
-				System.out.println("mapTask inside: " + Thread.currentThread().getName());
-				try {
-					for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-					    
-					
-					mapMethod(line); }
-//					placeInQueue(wordsInLine);
-				
-				System.out.println("Does it get past the while loop?");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
+		System.out.println("Counter threads shutdown.");
+		
+		System.out.println("finalPrint inside : " + Thread.currentThread().getName());
+		
+		for (Entry<String, AtomicInteger> entry : wordToCounts.entrySet()) {
+			System.out.println("Word: "+ entry.getKey() + " Count: " + entry.getValue());
+		}
+		
+		System.out.println("There are "+ wordToCounts.size() + " unique words!");
+		
+		
 	
-				
-			};
-			
-			Runnable reduceTask = () -> {
-				System.out.println("reduceTask inside : " + Thread.currentThread().getName());
-				System.out.println("Does it...");
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println("... wait?");
-				
-				List<String> wordsFromQueue1 = takeFromQueue(queue, wordsFromQueue);
-				reduce(wordsFromQueue1);
-			
-				
-			};
 			
 			
-			executorService.submit(mapTask);
-			executorService.submit(reduceTask);
+//			fileReadersPool.submit(mapTask);
+//			executorService.submit(reduceTask);
 //			scheduledService.schedule(finalPrint, 5, TimeUnit.SECONDS);
 			
-			
-			
-			
-			
-			
-			executorService.shutdown();
-			executorService.awaitTermination(10, TimeUnit.SECONDS);
-			
-	        
-			
-			System.out.println("finalPrint inside : " + Thread.currentThread().getName());
-			
-			for (Entry<String, Integer> entry : wordToCounts.entrySet()) {
-				System.out.println("Word: "+ entry.getKey() + " Count: " + entry.getValue());
-			}
-			
-				CountWords.countWords(path);
-				reader.close();
-		
+				
+//				reader.close();
+//		
 	
 		// I don't think reduce needs to be part of the words...?
 				// I think It only needs to be called on the queue...
